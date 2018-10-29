8761f7cba3b27a6dd4573933b2ac86532e581883
Rakuram, Sidartha <sidartha@gatech.edu>
2018-10-28 20:12:34 -0400

    Fix bug with LandingPage syntax

--- app/src/main/java/edu/gatech/cs2340/centsible/activities/LandingPageActivity.java
+++ app/src/main/java/edu/gatech/cs2340/centsible/activities/LandingPageActivity.java
@@ -47,14 +47,10 @@ public class LandingPageActivity extends AppCompatActivity {
                                 new AuthUI.IdpConfig.EmailBuilder().build(),
                                 new AuthUI.IdpConfig.PhoneBuilder().build(),
                                 new AuthUI.IdpConfig.AnonymousBuilder().build()))
-			.setLogo(R.mipmap.centsibleblack)
+			            .setLogo(R.mipmap.centsibleblack)
                         .setTheme(R.style.LoginTheme)
                         .build(),
                 RC_SIGN_IN);
-                                .build(),
-                        RC_SIGN_IN);
-            }
-        });
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
